1738c28c1362f8ab660fb8617e0cb34454654460
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-03-15 17:59:49 -0300

    Projeto finalizado para desktop

--- assets/src/js/turnOnOffBreak.js
+++ assets/src/js/turnOnOffBreak.js
@@ -6,9 +6,12 @@
     let on = document.getElementById("on");
     let offButton = document.getElementById("offButton");
     let offIMG = document.getElementById("offIMG");
+    let msgAlert = document.getElementById("msgAviso");
+
+
 
     function broken() {
-        return offIMG.src.indexOf('break') > -1;
+        return offIMG.src.endsWith('break');
     }
 
     on.addEventListener("click", () => {
@@ -25,7 +28,20 @@
     });
 
     offIMG.addEventListener("click", () => {
-        offIMG.src = "assets/src/img/break.svg";
-        return
+        let redirect = offIMG.src = "assets/src/img/break.svg";
+        msgAlert.innerHTML = 'Eu avisei, não é mesmo?<br> Atualize a página novamente'
+
+        if (redirect.endsWith('break.svg')) {
+            on.disabled = true;
+            on.style.backgroundColor = "#818181"
+            on.style.border = "#818181"
+            on.style.pointerEvents = "none";
+            offIMG.style.pointerEvents = "none";
+            offButton.disabled = true;
+            offButton.style
+            offButton.style.backgroundColor = "#818181"
+            offButton.style.border = "#818181"
+            offButton.style.pointerEvents = "none";
+        }
     });
 })()
